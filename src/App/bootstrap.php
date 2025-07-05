594d52cd62bb6f7fe003dc93ec12cdad02877cd4
CHTINA ABDESSATAR <Chtina.abdessatar@gmail.com>
2025-07-05 18:14:47 +0100

    Routing

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -6,5 +6,8 @@ require __DIR__."/../../vendor/autoload.php";
 use Framework\App;
 
 $app = new App();
+
+$app->get('/');
+dd($app);
 return $app;
 
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -4,8 +4,17 @@ declare(strict_types=1);
 namespace Framework;
 
 Class App{
+    private Router $router;
+
+    function __construct(){
+        $this->router = new Router(); 
+    }
     public function run(){
         echo "Application is Running...!";
+    }
+    public function add(string $path){
+        $this->router->add('GET',$path);
+
     }
 }
 
