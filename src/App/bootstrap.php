f22a3c308d5a266dc9de01afbeaa8f102fc76486
CHTINA ABDESSATAR <Chtina.abdessatar@gmail.com>
2025-07-12 20:23:35 +0100

    Template Engine

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 require __DIR__."/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controllers\HomeController;
 
 $app = new App();
 
-$app->get('/');
-dd($app);
+$app->get('/moneylog/public/', [HomeController::class , 'home']);
+
 return $app;
 
+ 
--- src/Framework/App.php
+++ src/Framework/App.php
@@ -9,11 +9,19 @@ Class App{
     function __construct(){
         $this->router = new Router(); 
     }
+
     public function run(){
-        echo "Application is Running...!";
+        
+        $path = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        $this->router->dispatch($path,$method);
+        
     }
-    public function get(string $path){
-        $this->router->add('GET',$path);
+
+    public function get(string $path , array $controller){
+
+        $this->router->add('GET',$path ,$controller);
 
     }
 }
