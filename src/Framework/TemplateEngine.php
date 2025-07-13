5a21be5f35dfceb68d9ec8832b7da2796645a1a4
CHTINA ABDESSATAR <Chtina.abdessatar@gmail.com>
2025-07-13 16:44:36 +0100

    homepage rendering

--- src/Framework/TemplateEngine.php
+++ src/Framework/TemplateEngine.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Framework;
 
-class TemplateEngine{
-
-}
+class TemplateEngine
+{
+    public function __construct(private string $basePath){
+        
+    }
+    public function render(string $template) {
+            include "{$this->basePath}/{$template}";
+    }
+}   
